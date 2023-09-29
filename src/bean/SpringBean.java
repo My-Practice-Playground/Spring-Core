3faa391e5950afa62c71b13b7d99f6814950ac87
Shan <perera.alc2000@gmail.com>
2023-09-29 10:28:24 +0530

    Application context register

--- src/bean/SpringBean.java
+++ src/bean/SpringBean.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class SpringBean implements BeanNameAware,BeanFactoryAware{
+public class SpringBean implements BeanNameAware,BeanFactoryAware, ApplicationContextAware {
     public SpringBean() {
         System.out.println("Spring bean Object created!");
     }
@@ -24,5 +26,10 @@ public class SpringBean implements BeanNameAware,BeanFactoryAware{
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("Bean Factory aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context register");
     }
 }
