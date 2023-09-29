3a3d39e654369774a43adde5e93650574a3e8b1c
Shan <perera.alc2000@gmail.com>
2023-09-29 10:26:15 +0530

    Bean name aware

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -4,8 +4,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ctx.close();
+            }
+        }));
         ctx.register(AppConfig.class);
         ctx.refresh();
-        ctx.close();
+
     }
 }
--- src/bean/SpringBean.java
+++ src/bean/SpringBean.java
@@ -1,10 +1,21 @@
 package bean;
 
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class SpringBean implements BeanNameAware {
     public SpringBean() {
         System.out.println("Spring bean Object created!");
+    }
+
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean name aware!");
     }
 }
