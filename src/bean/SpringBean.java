4b52e81a8c539a48a735234a33d3d707844fef90
Shan <perera.alc2000@gmail.com>
2023-09-29 10:37:04 +0530

    Post construct

--- src/bean/SpringBean.java
+++ src/bean/SpringBean.java
@@ -4,15 +4,18 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class SpringBean implements BeanNameAware,BeanFactoryAware, ApplicationContextAware {
+public class SpringBean implements BeanNameAware,BeanFactoryAware, ApplicationContextAware, InitializingBean {
     public SpringBean() {
         System.out.println("Spring bean Object created!");
     }
@@ -32,4 +35,10 @@ public class SpringBean implements BeanNameAware,BeanFactoryAware, ApplicationCo
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("Application context register");
     }
+    @PostConstruct
+    public void method(){
+        System.out.println("Post Construct");
+    }
+
 }
+
